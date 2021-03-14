6326e835537b05cb258ef760d5c6c0bc3acadcb2
denodaeus <bobby.smith@gmail.com>
2021-03-14 16:39:25 -0400

    add error handling around entry match

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,17 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => { 
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }
